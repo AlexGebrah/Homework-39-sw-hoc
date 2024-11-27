bf955bdda8187a61a77c492bb548064b66bfa9bf
HPPro <alexisgebrah@gmail.com>
2024-11-27 21:26:26 +0200

    sw routing

---
+++ src/hoc/withErrorPage.tsx
@@ -0,0 +1,25 @@
+import {characters, defaultHero} from "../utils/constants.ts";
+import {useParams} from "react-router-dom";
+import ErrorPage from "../components/ErrorPage.tsx";
+import {useContext, useEffect} from "react";
+import {SWContext} from "../utils/context.ts";
+
+export const SwWrapper = (WrapComponent: React.ComponentType) => {
+    return   () => {
+        const { heroId = defaultHero } = useParams();
+        const {changeHero} = useContext(SWContext);
+
+        useEffect(() => {
+            if (!characters[heroId]) {
+                return;
+            }
+            changeHero(heroId);
+        }, [heroId])
+
+        return characters[heroId] ? (
+            <WrapComponent  />
+            )
+            :
+            <ErrorPage/>
+    }
+};
